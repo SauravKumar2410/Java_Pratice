dc391fbd6a47218b359135535c1286dfb3c8bb71
skumar2410 <saurav.j.k.1997@gmail.com>
2023-08-02 22:29:22 +0530

    added cartpage

--- src/Feature_File/Add_Tag.java
+++ src/Feature_File/Add_Tag.java
@@ -6,6 +6,7 @@ public class Add_Tag {
 	public static void main(String[] args) {
 		
 		System.out.println("tag release");
+		System.out.println("tag release 2.0 ");
 	}
 
 }
---
+++ src/Feature_File/Cartpage.java
@@ -0,0 +1,10 @@
+package Feature_File;
+
+public class Cartpage {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+
+}
